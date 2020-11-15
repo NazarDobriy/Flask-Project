f9bf97d79bd1b6b2d3fb3878e8b8f35985719855
NazarDobriy <nazaranddobriy@gmail.com>
2020-11-16 00:14:24 +0200

    PP_lab_4

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from werkzeug.serving import run_simple
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/hello-world-6')
+def hello_world():
+    # return "Hello World! Варіант 6"
+    return render_template("index.html")
+
+
+server = run_simple('localhost', 5000, app, use_reloader=True)
+
+
+
